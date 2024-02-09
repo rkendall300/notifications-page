10125dbf6a4ac40c0ef599dc9afe1c89e2c18e36
Ryan Kendall <ryekendallion@gmail.com>
2024-02-08 23:06:23 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,70 +1,60 @@
-# Getting Started with Create React App
+# Frontend Mentor - Notifications page solution
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a solution to the [Notifications page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/notifications-page-DqK5QAmKbC). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
-## Available Scripts
+## Table of contents
 
-In the project directory, you can run:
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
 
-### `npm start`
+## Overview
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+### The challenge
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Users should be able to:
 
-### `npm test`
+- Distinguish between "unread" and "read" notifications
+- Select "Mark all as read" to toggle the visual state of the unread notifications and set the number of unread messages to zero
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Screenshot
 
-### `npm run build`
+## Desktop Design
+![](./desktop-design.PNG)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Mobile Design
+![](./mobile-design.PNG)
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### Links
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+- Live Site URL: [Notifications Page](https://rkendall300.github.io/notifications-page/)
 
-### `npm run eject`
+## My process
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+### Built with
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+### What I learned
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+This project was a little tricky getting all the different areas set up, but overall not too bad. I added a small bit of functionality to make the notification number at the top change depending on how many are still unread. I'm very happy with how the final product turned out.
 
-## Learn More
+## Author
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+- Frontend Mentor - [@rkendall300](https://www.frontendmentor.io/profile/rkendall300)
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+## Acknowledgments
 
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Shout out to my friend Eric for motivating me to get the higher level Frontend Mentor projects going!
--- src/App.js
+++ src/App.js
@@ -1,24 +1,120 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import avatarAngela from './assets/images/avatar-angela-gray.webp';
+import avatarAnna from './assets/images/avatar-anna-kim.webp';
+import avatarJacob from './assets/images/avatar-jacob-thompson.webp';
+import avatarKimberly from './assets/images/avatar-kimberly-smith.webp';
+import avatarMark from './assets/images/avatar-mark-webber.webp';
+import avatarNathan from './assets/images/avatar-nathan-peterson.webp';
+import avatarRizky from './assets/images/avatar-rizky-hasanuddin.webp';
+import chessImage from './assets/images/image-chess.webp';
 
 function App() {
+
+  const [notificationCounter, setNotificationCounter] = useState(3);
+  const [markRead, setMarkRead] = useState(false);
+  const [angelaRead, setAngelaRead] = useState(false);
+  const [jacobRead, setJacobRead] = useState(false);
+
+  const markAllAsRead = () => {
+    setMarkRead(true);
+    setAngelaRead(true);
+    setJacobRead(true);
+    setNotificationCounter(0);
+  }
+
+  const decrementCounter = () => {
+    if (notificationCounter !== 0) {
+      setNotificationCounter(notificationCounter - 1);
+    }
+  }
+
+  const markAsRead = (targetToMark) => {
+    switch (targetToMark) {
+      case "Mark":
+        setMarkRead(true);
+        decrementCounter();
+        break;
+      case "Angela":
+        setAngelaRead(true);
+        decrementCounter();
+        break;
+      case "Jacob":
+        setJacobRead(true);
+        decrementCounter();
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div id="backdrop">
+        <div id="card">
+          <div id="header">
+            <h1 id="title">Notifications</h1>
+            <div id="notification-number">{notificationCounter}</div>
+            <button id="mark-read" onClick={markAllAsRead}>Mark all as read</button>
+          </div>
+          <div id="notification-container">
+            <div className={markRead ? "notification" : "notification unread"} onClick={() => markAsRead("Mark")}>
+              <img src={avatarMark} alt="avatar" className="avatar"></img>
+              <div className="two-line">
+                <p className="notify-text"><span className="bold">Mark Webber</span> reacted to your recent post <span className="heavier">My first tournament today!</span><span className={markRead ? "hide" : "unread-marker"}></span></p>
+                <p className="time">1m ago</p>
+              </div>
+            </div>
+            <div className={angelaRead ? "notification" : "notification unread"} onClick={() => markAsRead("Angela")}>
+              <img src={avatarAngela} alt="avatar" className="avatar"></img>
+              <div className="two-line">
+                <p className="notify-text"><span className="bold">Angela Gray</span> followed you<span className={angelaRead ? "hide" : "unread-marker"}></span></p>
+                <p className="time">5m ago</p>
+              </div>
+            </div>
+            <div className={jacobRead ? "notification" : "notification unread"} onClick={() => markAsRead("Jacob")}>
+              <img src={avatarJacob} alt="avatar" className="avatar"></img>
+              <div className="two-line">
+                <p className="notify-text"><span className="bold">Jacob Thompson</span> has joined your group <span className="group-name">Chess Club</span><span className={jacobRead ? "hide" : "unread-marker"}></span></p>
+                <p className="time">1 day ago</p>
+              </div>
+            </div>
+            <div className="notification" id="rizky-space">
+              <img src={avatarRizky} alt="avatar" className="avatar"></img>
+              <div className="two-line">
+                <p className="notify-text"><span className="bold">Rizky Hasanuddin</span> sent you a private message</p>
+                <p className="time">5 days ago</p>
+              </div>
+              <div id="message-container">
+                <p id="private-message">Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.<span className="hide"></span></p>
+              </div>
+            </div>
+            <div className="notification">
+              <img src={avatarKimberly} alt="avatar" className="avatar"></img>
+              <div className="two-line">
+                <p className="notify-text" id="compact"><span className="bold">Kimberly Smith </span> commented on your picture<span className="hide"></span></p>
+                <p className="time">1 week ago</p>
+              </div>
+              <img src={chessImage} alt="chess" id="chess-image"></img>
+            </div>
+            <div className="notification">
+              <img src={avatarNathan} alt="avatar" className="avatar"></img>
+              <div className="two-line">
+                <p className="notify-text"><span className="bold">Nathan Peterson</span> reacted to your recent post <span className="heavier">5 end-game strategies to increase your win rate</span><span className="hide"></span></p>
+                <p className="time">2 weeks ago</p>
+              </div>
+            </div>
+            <div className="notification">
+              <img src={avatarAnna} alt="avatar" className="avatar"></img>
+              <div className="two-line">
+                <p className="notify-text"><span className="bold">Anna Kim</span> left the group <span className="group-name">Chess Club</span><span className="hide"></span></p>
+                <p className="time">2 weeks ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
